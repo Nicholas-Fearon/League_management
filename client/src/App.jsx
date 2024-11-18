f7d141fdf606f6416c02e6bd407ccd517035fcd8
Nicholas-Fearon <nejfearon@gmail.com>
2024-11-18 07:17:56 +0000

    pages path update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
-import Teams from "./pages/teams";
+import Teams from "./pages/Teams";
 import Rosters from "./pages/Rosters";
-
+import Players from "./pages/players";
 import Home from "./pages/Home";
+import Playerinfo from "./pages/Playerinfo";
 
 export default function App() {
   return (
@@ -14,7 +15,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/teams/:id" element={<Rosters />} />
-
+        <Route path="/players" element={<Players />}></Route>
+        <Route path="/playerinfo/:id" element={<Playerinfo />}></Route>
         <Route path="*" element={<p>404</p>} />
       </Routes>
     </>
